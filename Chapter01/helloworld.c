bcec4b88cbbc1d327f4e999268cc5d55beac46a8
Dixon <59415488+radixon@users.noreply.github.com>
2023-05-21 15:41:55 -0400

    Documentation
    Comment on how many threads are available on the processor this program was run on.

--- Chapter01/helloworld.c
+++ Chapter01/helloworld.c
@@ -1,6 +1,7 @@
 #include <stdio.h>
 #include <pthread.h>
 
+// AMD Ryzen 9 5900X 12-core, 24-thread Processor
 #define NUM_THREADS 24
 
 void* printHello(void* threadId) {
@@ -12,10 +13,10 @@ void* printHello(void* threadId) {
 int main() {
     pthread_t threads[NUM_THREADS];
     int rc;
-    size_t t;
+    size_t i;
 
-    for (t = 0; t < NUM_THREADS; t++) {
-        rc = pthread_create(&threads[t], NULL, printHello, (void*)t);
+    for (i = 0; t < NUM_THREADS; i++) {
+        rc = pthread_create(&threads[i], NULL, printHello, (void*)i);
         if (rc) {
             printf("ERROR: Return code from pthread_create() is %d\n", rc);
             return 1;
@@ -23,8 +24,8 @@ int main() {
     }
 
     // Wait for all threads to complete
-    for (t = 0; t < NUM_THREADS; t++) {
-        pthread_join(threads[t], NULL);
+    for (i = 0; t < NUM_THREADS; i++) {
+        pthread_join(threads[i], NULL);
     }
 
     return 0;
